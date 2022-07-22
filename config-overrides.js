da45ff7e8f12e8b2a78ef595451f1ae17e2c8490
Bart <bartolomej.kozorog@gmail.com>
2022-07-22 21:40:36 +0200

    WORKS: react@v17, cra@v5

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,34 @@
-module.exports = function override(webpackConfig) {
-    webpackConfig.module.rules.push({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: "javascript/auto",
-    });
-    return webpackConfig;
+const { ProvidePlugin } = require('webpack');
+
+module.exports = function (config, env) {
+    return {
+        ...config,
+        module: {
+            ...config.module,
+            rules: [
+                ...config.module.rules,
+                {
+                    test: /\.(m?js|ts)$/,
+                    enforce: 'pre',
+                    use: ['source-map-loader'],
+                },
+            ],
+        },
+        plugins: [
+            ...config.plugins,
+            new ProvidePlugin({
+                process: 'process/browser',
+            }),
+        ],
+        resolve: {
+            ...config.resolve,
+            fallback: {
+                assert: require.resolve('assert'),
+                buffer: require.resolve('buffer'),
+                stream: require.resolve('stream-browserify'),
+                crypto: require.resolve('crypto-browserify'),
+            },
+        },
+        ignoreWarnings: [/Failed to parse source map/],
+    };
 };
